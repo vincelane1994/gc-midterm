004c55a574693a790e47bb2b67f7cdea62f563ab
vincelane1994 <vincelane1994@gmail.com>
2019-07-15 13:50:40 -0400

    Added a method for adding new members

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MidTerm {
@@ -5,6 +10,23 @@ public class MidTerm {
 	static Scanner scnr = new Scanner(System.in);
 	public static void main(String[] args) {
 		boolean mainMenu = true;//As long as this is true it will keep looping the program
+		Path membersPath = Paths.get("members.txt");
+		if(Files.notExists(membersPath)) {
+			try {
+				Files.createFile(membersPath);
+			}catch (IOException e) {
+				System.out.println("Could not create file. " + e);
+			}
+		}
+		Path clubsPath = Paths.get("clubs.txt");
+		if(Files.notExists(clubsPath)) {
+			try {
+				Files.createFile(clubsPath);
+			}catch (IOException e) {
+				System.out.println("Could not create file. " + e);
+			}
+		}
+		
 		
 		do {
 			System.out.println("===Main Menu===");
@@ -20,7 +42,7 @@ public class MidTerm {
 			
 			switch(menuChoice) {
 			case 1://Add Member
-				System.out.println("This will be adding a member");
+				addMember(scnr);
 				break;
 			case 2://Remove Member
 				System.out.println("This will remove a member");
@@ -43,7 +65,47 @@ public class MidTerm {
 
 	}
 	
-	
+	public static void addMember(Scanner scnr) {//This method is to add new members to the clubs
+		boolean addNewMember = true;
+		int userChoice = 0;
+		do {
+			int id;
+			String name;
+			System.out.println("What type of member are you adding?\n1. Single Club Member\n2. Multi-Club Member\n3. Main Menu");
+			try {
+				userChoice = scnr.nextInt();
+			}catch(InputMismatchException ex) {
+				System.out.println("That was not a valid number.(Please enter a number 1-3)");
+			}
+			switch (userChoice) {
+				case 1://Single Club Member
+					System.out.print("What is the new member's name?: ");
+					name = scnr.nextLine();
+					System.out.print("What is the new Members id number?: ");
+					id = scnr.nextInt();
+					scnr.nextLine();
+					//SingleClubMember newSingleClubMember = new SingleClubMember(id, name);//This section is for adding new SingleClub members
+					break;
+				case 2://Multi-Club Member
+					System.out.print("What is the new member's name?: ");
+					name = scnr.nextLine();
+					System.out.print("What is the new Members id number?: ");
+					id = scnr.nextInt();
+					scnr.nextLine();
+					int points = 0;
+					//MultiClubMember newMultiClubMember = new MultiClubMember(id, name, points);//This section is for adding new MultiClub members
+					break;
+				case 3:
+					System.out.println("Exiting to main menu.");
+					addNewMember = false;
+					break;
+				default:
+					System.out.println("That was not a valid option");
+					break;
+					
+			}	
+		}while(addNewMember);
+	}
 	
 }
 				
