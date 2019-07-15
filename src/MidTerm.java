d5326824b90180940c43160742ca228de3d3fcd3
shehzad <slak786@gmail.com>
2019-07-15 10:31:54 -0400

    second line of the story

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,7 +3,7 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-
+       System.out.println("These monkeys really liked bananas. They ate bananans for breakfast, lunch and dinner!");
 	}
 
 }
