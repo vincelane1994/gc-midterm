fd2e33de1c060987f5fc72fd72aa6ee51fb028ab
vincelane1994 <vincelane1994@gmail.com>
2019-07-15 11:06:47 -0400

    cause error

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -2,9 +2,9 @@
 public class MidTerm {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");//Vince
-		System.out.println("These monkeys really liked bananas. They ate bananans for breakfast, lunch and dinner!");//Shehzad
-		System.out.println("These monkeys were lazy ninja monkeys, so they stole from the tourists to pay for their banana addiction.");
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of students.");//Vince
+		System.out.println("These students really liked bananas. They ate bananans for breakfast, lunch and dinner!");//Shehzad
+		System.out.println("These students were lazy students, so they stole from the tourists to pay for their banana addiction.");
 		System.out.println("The tourist really loved their bananas getting stolen too!");
 	}
 
