7574dcfb6ffe92766a4b03135fc65f8f4af7ab4c
shehzad <slak786@gmail.com>
2019-07-15 10:50:29 -0400

    second line of commit from me

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -5,6 +5,7 @@ public class MidTerm {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");//Vince
 		System.out.println("These monkeys really liked bananas. They ate bananans for breakfast, lunch and dinner!");//Shehzad
 		System.out.println("These monkeys were lazy ninja monkeys, so they stole from the tourists to pay for their banana addiction.");
+		System.out.println("The tourist really loved their bananas getting stolen too!");
 	}
 
 }
